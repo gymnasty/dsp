11dd5d2c2d1c32fabbbf623aee7a17fe5060768c
Masafumi Tominaga <gymnasty13@gmail.com>
2026-05-06 14:36:43 +0900

    スクロールをリセットするように

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App.tsx'
+import { ScrollToTop } from './components/ScrollToTop'
 import './index.css'
 import './i18n'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter>
+      <ScrollToTop />
       <App />
     </HashRouter>
   </React.StrictMode>,
